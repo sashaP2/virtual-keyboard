d131dde5d6c2912b6563665b9340c6feec64649e
Sasha Petushkova <sashpet_2000@mail.ru>
2023-04-30 12:58:32 +0300

    feat: the generation of DOM elements

---
+++ app.js
@@ -0,0 +1,82 @@
+document.body.insertAdjacentHTML("afterbegin", `<div class="wrapper">
+<h1>virtual-keyboard</h1>
+<textarea class="textarea" rows="10" cols="50"></textarea>
+<div class="keyboard">
+    <div class="row">
+        <div class="key">~</div>
+        <div class="key">1</div>
+        <div class="key">2</div>
+        <div class="key">3</div>
+        <div class="key">4</div>
+        <div class="key">5</div>
+        <div class="key">6</div>
+        <div class="key">7</div>
+        <div class="key">8</div>
+        <div class="key">9</div>
+        <div class="key">0</div>
+        <div class="key">-</div>
+        <div class="key">=</div>
+        <div class="key backspace">Backspace</div>
+    </div>
+    <div class="row">
+        <div class="key tab">Tab</div>
+        <div class="key">q</div>
+        <div class="key">w</div>
+        <div class="key">e</div>
+        <div class="key">r</div>
+        <div class="key">t</div>
+        <div class="key">y</div>
+        <div class="key">u</div>
+        <div class="key">i</div>
+        <div class="key">o</div>
+        <div class="key">p</div>
+        <div class="key">[</div>
+        <div class="key">]</div>
+        <div class="key">\</div>
+        <div class="key">Del</div>
+    </div>
+    <div class="row">
+        <div class="key capslock">CapsLock</div>
+        <div class="key">a</div>
+        <div class="key">s</div>
+        <div class="key">d</div>
+        <div class="key">f</div>
+        <div class="key">g</div>
+        <div class="key">h</div>
+        <div class="key">j</div>
+        <div class="key">k</div>
+        <div class="key">l</div>
+        <div class="key">;</div>
+        <div class="key">'</div>
+        <div class="key enter">Enter</div>
+    </div>
+    <div class="row">
+        <div class="key shift">Shift</div>
+        <div class="key">z</div>
+        <div class="key">x</div>
+        <div class="key">c</div>
+        <div class="key">v</div>
+        <div class="key">b</div>
+        <div class="key">n</div>
+        <div class="key">m</div>
+        <div class="key">,</div>
+        <div class="key">.</div>
+        <div class="key">/</div>
+        <div class="key">▲</div>
+        <div class="key shift">Shift</div>
+    </div>
+    <div class="row">
+        <div class="key ctrl">Ctrl</div>
+        <div class="key">Win</div>
+        <div class="key">Alt</div>
+        <div class="key space"></div>
+        <div class="key">Alt</div>
+        <div class="key">◄</div>
+        <div class="key">▼</div>
+        <div class="key">►</div>
+        <div class="key ctrl">Ctrl</div>
+    </div>
+</div>
+<p>Клавиатура создана в операционной системе Windows</p>
+<p>Для переключения языка комбинация: левые ctrl + alt</p>
+</div> `);
